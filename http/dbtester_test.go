302978d150410cef96cc8a0dda5d5700c807b9a8
Jim McBeath <git@j.jimmc.org>
2019-05-11 14:23:41 -0700

    Add some tests for http with db.

---
+++ http/dbtester_test.go
@@ -0,0 +1,70 @@
+package http_test
+
+import (
+  "database/sql"
+  "fmt"
+  "net/http"
+  "testing"
+
+  goldenhttp "github.com/jimmc/golden/http"
+)
+
+type dbhandler struct {
+  db *sql.DB
+}
+
+type exampleRow struct {
+  s string
+  n int
+}
+
+func readDbRows(db *sql.DB) ([]exampleRow, error) {
+  sql := "SELECT s, n FROM test ORDER BY s;"
+  rows, err := db.Query(sql)
+  if err != nil {
+    return nil, err
+  }
+  defer rows.Close()
+  xRows := []exampleRow{}
+  for rows.Next() {
+    var s string
+    var n int
+    err := rows.Scan(&s, &n)
+    if err != nil {
+      return nil, err
+    }
+    xRow := exampleRow{s: s, n: n}
+    xRows = append(xRows, xRow)
+  }
+  if err := rows.Err(); err != nil {
+    return nil, err
+  }
+  return xRows, nil
+}
+
+func (h *dbhandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+  xRows, err := readDbRows(h.db)
+  if err != nil {
+    http.Error(w, fmt.Sprintf("Error reading database: %v", err), http.StatusInternalServerError)
+    return
+  }
+  w.WriteHeader(http.StatusOK)
+  for _, xRow := range xRows {
+    s := fmt.Sprintf("%v\n", xRow)
+    w.Write([]byte(s))
+  }
+}
+
+func TestHttpDbTester(t *testing.T) {
+  request := func() (*http.Request, error) {
+    return http.NewRequest("GET", "/api/foo/", nil)
+  }
+  r := goldenhttp.NewTester(func (r *goldenhttp.Tester) http.Handler {
+    h := &dbhandler{}
+    h.db = r.DB
+    return h
+  })
+  if err := r.Run("foo-db", request); err != nil {
+    t.Fatalf("Error in Run: %s", err)
+  }
+}
