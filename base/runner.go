7122e1ec2b4218042638e76e7eea4a2fc2cc2cd7
Jim McBeath <git@j.jimmc.org>
2020-01-13 16:02:46 -0800

    Split Runner into Runner and MultiRunner.

--- base/runner.go
+++ base/runner.go
@@ -6,9 +6,6 @@ import (
 
 // Runner defines the methods used when running one of our unit tests.
 type Runner interface {
-  // Init does one-time initialization of this Runner.
-  Init() error
-
   // Arrange sets up for one test, to be performed by Act().
   Arrange() error
 
@@ -17,18 +14,24 @@ type Runner interface {
 
   // Assert checks the output against the golden file.
   Assert() error
+}
+
+// MultiRunner defines the methods used when running multiple tests that
+// include common setup and close steps.
+// Typical use for a MultiRunner is to call m.Init(), then do some test-specific
+// setup work and call RunTest, repeat that for all tests, and finish by calling m.Close().
+type MultiRunner interface {
+  Runner
+
+  // Init does one-time initialization of this Runner before running tests.
+  Init() error
 
   // Close cleans everything up. Nothing else can be called after Close.
   Close() error
 }
 
-// Run runs one test on the Runner.
-func Run(r Runner) error {
-  // Do the one-time initialization.
-  if err := r.Init(); err != nil {
-    return err
-  }
-
+// RunTest runs one test on the Runner.
+func RunTest(r Runner) error {
   // Set things up for our one test.
   if err := r.Arrange(); err != nil {
     return err
@@ -40,12 +43,33 @@ func Run(r Runner) error {
   }
 
   // Check the output against the golden file.
-  if err := r.Assert(); err != nil {
+  return r.Assert()
+}
+
+// Run runs one test on the Runner.
+// If the Runner is also a MultiRunner, this also runs the Init and Close functions.
+// If not, this is the same as calling RunTest.
+func Run(r Runner) error {
+  if m, ok := r.(MultiRunner); ok {
+    // Do the one-time initialization.
+    if err := m.Init(); err != nil {
+      return err
+    }
+  }
+
+  // Run one test.
+  if err := RunTest(r); err != nil {
     return err
   }
 
-  // Clean up.
-  return r.Close()
+  if m, ok := r.(MultiRunner); ok {
+    // Clean up.
+    if err := m.Close(); err != nil {
+      return err
+    }
+  }
+
+  return nil
 }
 
 // FatalIfError calls testing.T.Fatal if there is an error.
